885ee44e843ea00a2eb60635519746f753413fc7
mackycr1 <mackycr1@gmail.com>
2021-11-19 20:01:15 -0600

    Deletes BD as it was giving error when referencing entity

--- WBL/DataAccess.cs
+++ WBL/DataAccess.cs
@@ -9,20 +9,6 @@ using Entity;
 
 namespace BD
 {
-    public interface IDataAccess
-    {
-        Task<DBEntity> ExecuteAsync(string sp, object Param = null, int? Timeout = null);
-        Task<IEnumerable<dynamic>> QueryAsync(string sp, object Param = null, int? Timeout = null);
-        Task<IEnumerable<T>> QueryAsync<T, B, C, D, E, F, G>(string sp, string split, object Param = null, int? Timeout = null);
-        Task<IEnumerable<T>> QueryAsync<T, B, C, D, E, F>(string sp, string split, object Param = null, int? Timeout = null);
-        Task<IEnumerable<T>> QueryAsync<T, B, C, D, E>(string sp, string split, object Param = null, int? Timeout = null);
-        Task<IEnumerable<T>> QueryAsync<T, B, C, D>(string sp, string split, object Param = null, int? Timeout = null);
-        Task<IEnumerable<T>> QueryAsync<T, B, C>(string sp, string split, object Param = null, int? Timeout = null);
-        Task<IEnumerable<T>> QueryAsync<T, B>(string sp, string split, object Param = null, int? Timeout = null);
-        Task<IEnumerable<T>> QueryAsync<T>(string sp, object Param = null, int? Timeout = null);
-        Task<T> QueryFirstAsync<T>(string sp, object Param = null, int? Timeout = null);
-    }
-
 
     public class DataAccess : IDataAccess
     {
