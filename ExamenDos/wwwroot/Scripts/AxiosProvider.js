bff6fea49fc42c217528e93377d779d427e4c0ce
mackycr1 <mackycr1@gmail.com>
2021-11-19 22:05:41 -0600

    Delete and Gride Code created

--- ExamenDos/wwwroot/Scripts/AxiosProvider.js
+++ ExamenDos/wwwroot/Scripts/AxiosProvider.js
@@ -1,2 +1,17 @@
 "use strict";
+var App;
+(function (App) {
+    var AxiosProvider;
+    (function (AxiosProvider) {
+        //export const GuardarEmpleado = () => axios.get<Entity.DBEntity>("aplicacion").then(({data})=>data );
+        AxiosProvider.ProductoEliminar = function (id) { return axios.delete("Producto/Grid?handler=Delete&id=" + id).then(function (_a) {
+            var data = _a.data;
+            return data;
+        }); };
+        AxiosProvider.OrdenEliminar = function (id) { return axios.delete("Orden/Grid?handler=Delete&id=" + id).then(function (_a) {
+            var data = _a.data;
+            return data;
+        }); };
+    })(AxiosProvider = App.AxiosProvider || (App.AxiosProvider = {}));
+})(App || (App = {}));
 //# sourceMappingURL=AxiosProvider.js.map
--- ExamenDos/wwwroot/Scripts/AxiosProvider.ts
+++ ExamenDos/wwwroot/Scripts/AxiosProvider.ts
@@ -2,10 +2,6 @@
 namespace App.AxiosProvider   {
 
     //export const GuardarEmpleado = () => axios.get<Entity.DBEntity>("aplicacion").then(({data})=>data );
-
-
+    export const ProductoEliminar = (id) => axios.delete<DBEntity>("Producto/Grid?handler=Delete&id=" + id).then(({ data }) => data);
+    export const OrdenEliminar = (id) => axios.delete<DBEntity>("Orden/Grid?handler=Delete&id=" + id).then(({ data }) => data);
 }
-
-
-
-
